c9ba17189927a085230ba285d557c202adb45a78
Anastasia Korotkova <stasya004@list.ru>
2025-08-22 01:38:04 +0300

    При запуске линтеров Flake 8 и mypy выдается не более 4 ошибок

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,8 +1,8 @@
 import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
 from src.processering import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 
 # Фикстуры для тестовых данных
